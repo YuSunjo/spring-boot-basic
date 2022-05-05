885527fec7a2abed46c5e78c8fad926e4ac670d6
YuSunjo <tnswh2023@gmail.com>
2022-05-05 22:20:45 +0900

    feat(spring): spring 으로 변경

--- src/main/java/com/example/springbasic/AppConfig.java
+++ src/main/java/com/example/springbasic/AppConfig.java
@@ -6,25 +6,32 @@ import com.example.springbasic.member.MemberServiceImpl;
 import com.example.springbasic.member.MemoryMemberRepository;
 import com.example.springbasic.order.OrderService;
 import com.example.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     /**
      * 구현 객체를 생성해줌
      * 생성자를 통해 주입
      */
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), getDiscountPolicy());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private RateDiscountPolicy getDiscountPolicy() {
+    @Bean
+    public RateDiscountPolicy getDiscountPolicy() {
         return new RateDiscountPolicy();
     }
 
