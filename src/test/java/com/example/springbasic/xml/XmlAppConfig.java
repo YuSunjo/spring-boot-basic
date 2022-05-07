b3e329c46b4d00ec150bd901d6a1f1a2ccd7d06a
YuSunjo <tnswh2023@gmail.com>
2022-05-07 13:41:46 +0900

    feat(xml): xml 기반 설정

---
+++ src/test/java/com/example/springbasic/xml/XmlAppConfig.java
@@ -0,0 +1,24 @@
+package com.example.springbasic.xml;
+
+import com.example.springbasic.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppConfig {
+
+    @Test
+    void xmlAppContext() {
+        // given
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+        // when
+
+        // then
+    }
+
+}
