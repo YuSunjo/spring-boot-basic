6cb74ec368f21dfc611a260b6fd4646c47deceec
YuSunjo <tnswh2023@gmail.com>
2022-05-11 21:44:07 +0900

    feat(bean): bean의 라이프 사이클

---
+++ src/test/java/com/example/springbasic/liveCycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package com.example.springbasic.liveCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycle() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient bean = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http:....");
+            return networkClient;
+        }
+    }
+
+}
