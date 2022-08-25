f756333df5515504b8bc74de373d8159f30ce02e
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-08-25 09:05:17 +0900

    feat(mvc2): http 메세지 컨버터

--- src/main/java/com/example/springbasic/mvc_2/web/validation/ValidationItemApiController.java
+++ src/main/java/com/example/springbasic/mvc_2/web/validation/ValidationItemApiController.java
@@ -6,6 +6,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * http 메세지 컨버터
+ */
 @Slf4j
 @RestController
 @RequestMapping("/validation/api/items")
